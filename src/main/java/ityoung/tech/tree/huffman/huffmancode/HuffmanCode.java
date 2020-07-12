b8a1c27b9e3ffea70ecd79100ba88599d1f3dde1
wangyang <wy13093741767@gmail.com>
2020-07-12 14:58:32 +0800

    auto commit

--- src/main/java/ityoung/tech/tree/huffman/huffmancode/HuffmanCode.java
+++ src/main/java/ityoung/tech/tree/huffman/huffmancode/HuffmanCode.java
@@ -1,29 +1,97 @@
 package ityoung.tech.tree.huffman.huffmancode;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 
+@Slf4j
 public class HuffmanCode {
 
     public static void main(String[] args) {
-        int i = (byte)Integer.parseInt("10101000", 2);
-        int j = (byte) 168;
-        System.out.println(i);
-        String content = "i like like like java do you like a java";
+        String content = "i like like like java do you like a java? fuck you";
         byte[] contentBytes = content.getBytes();
+        byte[] zip = huffmanZip(contentBytes);
+        log.info(Arrays.toString(zip));
+        StringBuilder bitStringBuilder = new StringBuilder();
+        int count = 0;
+        for (byte b : zip) {
+            String bitString;
+            if (count < zip.length - 1) {
+                bitString = byteToBitString(b, true);
+            } else {
+                bitString = byteToBitString(b, false);
+            }
+            bitStringBuilder.append(bitString);
+            count++;
+        }
+        log.info(bitStringBuilder.toString());
+        String s = fromBitStringToContent(bitStringBuilder.toString());
+        log.info(s);
+    }
+
+    public static String fromBitStringToContent(String bitString) {
+        Map<String, Byte> huffmanUnzipMap = new HashMap<>();
+        for (Map.Entry<Byte, String> entry : huffmanCodeMap.entrySet()) {
+            huffmanUnzipMap.put(entry.getValue(), entry.getKey());
+        }
+        StringBuilder origin = new StringBuilder();
+        int codeLength = 0;
+        for (int i = 0; i < bitString.length(); i += codeLength) {
+            codeLength = 1;
+            while (huffmanUnzipMap.get(bitString.substring(i, i + codeLength)) == null) {
+                codeLength++;
+            }
+            origin.append((char)(int)huffmanUnzipMap.get(bitString.substring(i, i + codeLength)));
+        }
+        return origin.toString();
+    }
+
+    public static String byteToBitString(byte b, boolean flag) {
+        int temp = b;
+        if (flag) {
+            temp |= 256;
+        }
+        String binaryString = Integer.toBinaryString(temp);
+        if (flag) {
+            return binaryString.substring(binaryString.length() - 8);
+        }
+        return binaryString;
+    }
+
+    public static byte[] huffmanZip(byte[] contentBytes) {
         List<Node> nodes = getNodes(contentBytes);
         Collections.sort(nodes);
         Node huffmanTree = createHuffmanTree(nodes);
-        huffmanTree.preOrder();
+//        huffmanTree.preOrder();
         getCodesMap(huffmanTree, "", stringBuilder);
-        System.out.println(huffmanCodeMap);
+        byte[] zip = zip(contentBytes, huffmanCodeMap);
+        return zip;
+    }
+
+    public static byte[] zip(byte[] bytes, Map huffmanCodeMap) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (byte contentByte : contentBytes) {
+        for (byte contentByte : bytes) {
             stringBuilder.append(huffmanCodeMap.get(contentByte));
         }
-        System.out.println(stringBuilder.toString().length());
-
+        log.info(stringBuilder.toString());
+        int length;
+        if (stringBuilder.length() % 8 == 0) {
+            length = stringBuilder.length() / 8;
+        } else {
+            length = stringBuilder.length() / 8 + 1;
+        }
+        byte[] huffmanCodeBytes = new byte[length];
+        int index = 0;
+        for (int i = 0; i < stringBuilder.length(); i += 8) {
+            if (i + 8 > stringBuilder.length()) {
+                huffmanCodeBytes[index] = (byte) Integer.parseInt(stringBuilder.substring(i), 2);
+            } else {
+                huffmanCodeBytes[index] = (byte) Integer.parseInt(stringBuilder.substring(i, i + 8), 2);
+            }
+            index++;
+        }
+        return huffmanCodeBytes;
     }
 
     static Map<Byte, String> huffmanCodeMap = new HashMap<>();
@@ -31,7 +99,6 @@ public class HuffmanCode {
 
 
     public static void getCodesMap(Node node, String code, StringBuilder stringBuilder) {
-//        stringBuilder.append(code);
         StringBuilder stringBuilder2 = new StringBuilder(stringBuilder);
         stringBuilder2.append(code);
         if (node != null) {
